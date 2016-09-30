a92f08647666b0b45c54e519876a381f6b9d695a
Danil Kovalev <kovalevdaad@gmail.com>
2016-09-30 17:51:39 +0300

    clear project

--- module/module.cpp
+++ module/module.cpp
@@ -2,8 +2,6 @@
 
 #include <security/pam_modules.h>
 
-#include "PamConversation.h"
-
 int pam_sm_open_session(pam_handle_t *pamh, int flags,
                                    int argc, const char **argv)
 {
